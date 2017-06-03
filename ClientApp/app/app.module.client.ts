2b65af27e51d8918c0e87c73c65d40bec458f46c
LAV <litva90@gmail.com>
2017-06-03 21:55:16 +1000

    configured header

--- ClientApp/app/app.module.client.ts
+++ ClientApp/app/app.module.client.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { sharedConfig } from './app.module.shared';
 
+
 @NgModule({
     bootstrap: sharedConfig.bootstrap,
     declarations: sharedConfig.declarations,
